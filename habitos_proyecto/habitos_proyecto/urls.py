94fd85c3696a219ad67fb6d90fd0af732d060756
JoseChavarria24 <azms.34@gmail.com>
2025-05-27 00:14:33 -0600

    API para recordatorios

--- habitos_proyecto/habitos_proyecto/urls.py
+++ habitos_proyecto/habitos_proyecto/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', index, name='index'),
+    path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
