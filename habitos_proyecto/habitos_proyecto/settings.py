81bee5e5e67026d4390bfbfdb99beec44885f7a6
JoseChavarria24 <azms.34@gmail.com>
2025-05-27 20:45:18 -0600

    Intento despliegue

--- habitos_proyecto/habitos_proyecto/settings.py
+++ habitos_proyecto/habitos_proyecto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nw527-m7p_a!b1vyuqx4%@!6twze^6f=x2wp+vof##e2zr!rok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
